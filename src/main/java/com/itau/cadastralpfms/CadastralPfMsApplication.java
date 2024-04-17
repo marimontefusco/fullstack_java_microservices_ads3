122a9499f0ffe72281d90172018d9137a315242b
marimontefusco <mari.montefusco@gmail.com>
2024-04-17 19:54:51 -0300

    repo inicial - first commit

---
+++ src/main/java/com/itau/cadastralpfms/CadastralPfMsApplication.java
@@ -0,0 +1,13 @@
+package com.itau.cadastralpfms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastralPfMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastralPfMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/cadastralpfms/CadastralPfMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.cadastralpfms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastralPfMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
